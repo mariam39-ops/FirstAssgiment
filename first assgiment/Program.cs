5c772c80ffe133beb9e00687fa06b1c0c84045f6
Mariam <mariamahmedmostafa18@gmail.com>
2026-09-04 20:12:20 +0300

    answer 2

--- first assgiment/Program.cs
+++ first assgiment/Program.cs
@@ -8,6 +8,13 @@
             Book book1 = new Book();
             object obj = book1;
             Console.WriteLine(obj);
+            //answer 2
+            Console.WriteLine(book1.ToString());
+            Console.WriteLine(book1.Equals(book1));
+            Console.WriteLine(book1.GetHashCode());
+            Console.WriteLine(book1.GetType());
+
         }
+
     }
 }
